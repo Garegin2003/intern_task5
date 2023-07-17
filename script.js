228c3bb759d7f7bd4fed953cf30a8e6aef6a490b
Garegin2003 <garehin.hakobyan.s@gmail.com>
2023-07-17 10:23:40 +0300

    final fixes

--- script.js
+++ script.js
@@ -2,85 +2,76 @@ function Masonry() {
   this.resizeRequestId = null;
 }
 
-Masonry.prototype.render = function(className, objectSettings) {
-
-  const container = document.querySelector(className)
-  const items = document.querySelectorAll('.masonry__item')
-  const columnWidth = objectSettings.columnWidth || 200
-  const autoResize = objectSettings.autoResize || false
-  const gap = objectSettings.gap || 0
-  const numberColumns = Math.trunc(container.offsetWidth / columnWidth)
-  const columns = []
+Masonry.prototype.render = function (className, objectSettings) {
+  const container = document.querySelector(className);
+  const items = document.querySelectorAll('.masonry__item');
+  const columnWidth = objectSettings.columnWidth || 200;
+  const autoResize = objectSettings.autoResize || false;
+  const gap = objectSettings.gap || 0;
+  const numberColumns = Math.trunc(container.offsetWidth / columnWidth);
+  const columns = [];
 
   for (let i = 0; i < numberColumns; i++) {
-    columns.push(0)
+    columns.push(0);
   }
 
   for (let i = 0; i < items.length; i++) {
-    const item = items[i]
-    const columnIndex = getColumnIndex(columns)
-    
-    item.style.left = `${(columnIndex * columnWidth) + (columnIndex * gap)}px`
-    item.style.top = `${columns[columnIndex]}px`
-    item.style.width = columnWidth + 'px'
-    columns[columnIndex] += item.offsetHeight + gap
+    const item = items[i];
+    const columnIndex = getColumnIndex(columns);
+
+    item.style.left = `${columnIndex * columnWidth + columnIndex * gap}px`;
+    item.style.top = `${columns[columnIndex]}px`;
+    item.style.width = columnWidth + 'px';
+    columns[columnIndex] += item.offsetHeight + gap;
   }
 
+  container.style.height = Math.max(...columns) + 'px';
 
-
-  container.style.height = Math.max(...columns) +  'px'
-  
   if (autoResize) {
     window.addEventListener('resize', () => {
-
       if (this.resizeRequestId) {
-        return; 
+        return;
       }
 
       this.resizeRequestId = window.requestAnimationFrame(() => {
-        console.log(565);
         this.resizeRequestId = null;
 
         this.handleResize(className, {
           columnWidth: columnWidth,
           autoResize: autoResize,
-          gap: gap
+          gap: gap,
         });
       });
     });
   } else {
     window.removeEventListener('resize', this.handleResize);
   }
-}
+};
 
 function getColumnIndex(columns) {
-  let index = 0
-  let indexHeight = columns[0]
+  let index = 0;
+  let indexHeight = columns[0];
 
   for (let i = 0; i < columns.length; i++) {
-
     if (columns[i] <= indexHeight) {
-      index = i
-      indexHeight = columns[i]
+      index = i;
+      indexHeight = columns[i];
     }
   }
 
-  return index
-
+  return index;
 }
 
-Masonry.prototype.handleResize = function(className, objectSettings) {
+Masonry.prototype.handleResize = function (className, objectSettings) {
   this.render(className, objectSettings);
 };
 
 const MasonryLayout = new Masonry();
 
 window.addEventListener('DOMContentLoaded', () => {
-
   MasonryLayout.render('.masonry', {
     columnWidth: 300,
     autoResize: true,
-    gap: 10
+    gap: 10,
   });
-
 });
